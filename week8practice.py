890904b2ca5dee58f7502b754df51ab4390ae684
rachelki <71536809+rachelki@users.noreply.github.com>
2021-05-18 22:07:13 -0700

    Update week8practice.py

--- week8practice.py
+++ week8practice.py
@@ -4,7 +4,9 @@ def last_chars(word,num):
     num2 = [0]
     num1 = ''
     num1 = ''.join(word)
-    if(len(word[-1]) <= num):
+    if word == []:
+        num2 = []
+    elif(len(word[-1]) <= num):
         num2 = []
     else:
         num2[0] = num1[-num:]
